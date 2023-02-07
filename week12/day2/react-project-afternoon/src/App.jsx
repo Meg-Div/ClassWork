fd650dbccace02afd63233d62fed37914903ce42
Megan <meganthielman@gmail.com>
2023-02-07 07:19:32 -0800

    Project update

--- week12/day2/react-project-afternoon/src/App.jsx
+++ week12/day2/react-project-afternoon/src/App.jsx
@@ -4,28 +4,10 @@ import { employeeData } from "../src/components/employeeData";
 import { EmployeeContainer } from "./components/EmployeeContainer/EmployeeContainer";
 
 function App() {
-  //setting state as employeeData
-  const [employees, setEmployee] = useState(employeeData);
 
-  //filtering employeeData
-  const filteredEmployeeFromList = (person) => {
-
-    //filtering any employees out who have the same name
-    const filteredList = employees.filter(
-      (employee) => employee.first_name !== person.first_name
-    );
-
-    //setting new state to filtered list
-    setEmployee(filteredList);
-
-  }
-  //passing the data and function through to the Employee Container
   return (
     <div className="App">
-      <EmployeeContainer 
-          employeeData={employees}
-          filteredEmployeeFromList={filteredEmployeeFromList}
-      />
+      <h1>Howdy</h1>
     </div>
   );
 }
